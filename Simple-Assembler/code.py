581a5137deb9a6e5346fd5b0e17018ca1d84faf7
Kush Aggarwal <kushaggarwal@Kushs-MacBook-Pro.local>
2021-08-12 22:57:42 +0530

    error partially

--- Simple-Assembler/code.py
+++ Simple-Assembler/code.py
@@ -1,7 +1,6 @@
 opcode={"add":"00000","sub":"00001","mov":"00010","ld":"00100","st":"00101","mul":"00110","div":"00111","rs":"01000","ls":"01001","xor":"01010","or":"01011","and":"01100","not":"01101","cmp":"01110","jmp":"01111","jlt":"10000","jgt":"10001","je":"10010","hlt":"10011"}
 register_address={"r0":"000","r1":"001","r2":"010","r3":"011","r4":"100","r5":"101","r6":"110","flag":"111"}
 
-
 def decimalToBinary(n):
     x = bin(n).replace("0b", "")
     y = len(x)
@@ -31,7 +30,120 @@ def typeF(op):
     a = opcode(op) + "00000000000"
     return a
 
-#def checkError():
-    
+def checkTypeA(arr):
+    if len(arr)!=4:
+        return False
+
+    if arr[1] not in register_address.keys():
+        return False
+    if arr[2] not in register_address.keys():
+        return False
+    if arr[3] not in register_address.keys():
+        return False
+    return True
+
+
+def checkTypeB(arr):
+    if len(arr)!=3:
+        return False
+    if arr[1] not in register_address.keys():
+        return False
+    if type(arr[2]) != int:
+        return False
+
+    if type(arr[2]) == int:
+        if arr[2]>255 or arr[2]<0:
+            return False
+
+    return True
+
+def checkTypeC(arr):
+    if len(arr)!=3:
+        return False
+    if arr[1] not in register_address.keys():
+        return False
+    if arr[2] not in register_address.keys():
+        return False
+    return True
+
+def checkTypeD(arr):
+
+    return
+
+def checkTypeE(arr):
+
+    return
+
+def checkTypeF(arr):
+    if len(arr) != 1:
+        return False
+    return True
+
+
+def checkError(arr):
+    if arr[0] == "add":
+        return checkTypeA(arr)
+    elif arr[0] == "sub":
+        return checkTypeA(arr)
+    elif arr[0] == "mov":
+        return checkTypeB(arr) or checkTypeC
+    elif arr[0] == "ld":
+        return checkTypeD(arr)
+    elif arr[0] == "st":
+        return checkTypeD(arr)
+    elif arr[0] == "mul":
+        return checkTypeA(arr)
+    elif arr[0] == "div":
+        return checkTypeC(arr)
+    elif arr[0] == "rs":
+        return checkTypeB(arr)
+    elif arr[0] == "ls":
+        return checkTypeB(arr)
+    elif arr[0] == "xor":
+        return checkTypeA(arr)
+    elif arr[0] == "or":
+        return checkTypeA(arr)
+    elif arr[0] == "and":
+        return checkTypeA(arr)
+    elif arr[0] == "not":
+        return checkTypeC(arr)
+    elif arr[0] == "cmp":
+        return checkTypeC(arr)
+    elif arr[0] == "jmp":
+        return checkTypeE(arr)
+    elif arr[0] == "jlt":
+        return checkTypeE(arr)
+    elif arr[0] == "jgt":
+        return checkTypeE(arr)
+    elif arr[0] == "je":
+        return checkTypeE(arr)
+    elif arr[0] == "hlt":
+        return checkTypeF(arr)
+    else:
+        return False
+
+
+def main():
+    instruction_list = []
+    var_list = []
+    binary_out = []
+    while True:
+        try:
+            line = input()
+            instruction_list.append(line)
+
+        except EOFError:
+            break
+    for i in instruction_list:
+        arr = i.split(" ")
+        if arr[0] == 'var':
+            instruction_list.append(instruction_list.pop())
+
+
+
+
+
+if __name__== "__main__":
+    main()
 
 
