fbf1015ed25583f275159a91a53bfd6c349dbaf3
Kush Aggarwal <kushaggarwal@Kushs-MacBook-Pro.local>
2021-08-12 21:48:54 +0530

    types added

--- Simple-Assembler/code.py
+++ Simple-Assembler/code.py
@@ -2,7 +2,35 @@ opcode={"add":"00000","sub":"00001","mov":"00010","ld":"00100","st":"00101","mul
 register_address={"r0":"000","r1":"001","r2":"010","r3":"011","r4":"100","r5":"101","r6":"110","flag":"111"}
 
 
+def decimalToBinary(n):
+    x = bin(n).replace("0b", "")
+    y = len(x)
+    z = (8-y)*"0" + str(x)
+    return z;
+
 def typeA(op,reg1,reg2,reg3):
     a=opcode(op)+"00"+register_address(reg1)+register_address(reg2)+register_address(reg3)
     return a
+
+def typeB(op, reg1, val):
+    a = opcode(op) + register_address(reg1) + decimalToBinary(val)
+    return a;
+def typeC(op, reg1, reg2):
+    a = opcode(op) + "00000" + register_address(reg1) + register_address(reg2)
+    return a;
+
+def typeD(op, reg1, mem):
+    a = opcode(op) + register_address(reg1) + "memory"
+    return a;
+
+def typeE(op, mem):
+    a = opcode(op) + "000" + "memory"
+    return a;
+
+def typeF(op):
+    a = opcode(op) + "00000000000"
+    return a;
+
+def checkError()
+
 
