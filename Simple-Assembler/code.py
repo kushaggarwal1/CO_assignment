045da87a9b1ebfbb9f0b7acc5461dbc0fda6bc11
shreyanshu <admin@Pixter45.local>
2021-08-13 18:48:40 +0700

    white space removal method

--- Simple-Assembler/code.py
+++ Simple-Assembler/code.py
@@ -1,3 +1,6 @@
+import re
+
+
 opcode={"add":"00000","sub":"00001","mov":"00010","ld":"00100","st":"00101","mul":"00110","div":"00111","rs":"01000","ls":"01001","xor":"01010","or":"01011","and":"01100","not":"01101","cmp":"01110","jmp":"01111","jlt":"10000","jgt":"10001","je":"10010","hlt":"10011"}
 register_address={"r0":"000","r1":"001","r2":"010","r3":"011","r4":"100","r5":"101","r6":"110","flag":"111"}
 label_instructions={}
@@ -152,6 +155,9 @@ def main():
         if arr[0] == 'var':
             var_addr(arr[1])=i
     
+def extraSpaceRemoval(a):       #s-->string      
+    return re.sub(' +',' ', a)  #this removes the excess white spaces in the string
+        
 
 
 
